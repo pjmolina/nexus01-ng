ea6fdc4ed4060cc2944fcd7b720fe927d61ebf4d
Pedro J. Molina <pjmolina@gmail.com>
2023-09-14 17:01:13 +0200

    patron de desuscripción

--- src/app/planets/planet-detail/planet-detail.component.ts
+++ src/app/planets/planet-detail/planet-detail.component.ts
@@ -1,23 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Planet } from 'src/app/domain/planet';
+import { LoggerService } from 'src/app/services/logger.service';
 import { PlanetService } from 'src/app/services/planet.service';
-
 @Component({
   selector: 'app-planet-detail',
   templateUrl: './planet-detail.component.html',
   styleUrls: ['./planet-detail.component.scss']
 })
-export class PlanetDetailComponent implements OnInit {
+export class PlanetDetailComponent implements OnInit, OnDestroy {
   @Input() id?: number;
   planet?: Planet;
   error = '';
+  sub?: Subscription;
 
-  constructor(private planetService: PlanetService) {
+  constructor(private log: LoggerService, private planetService: PlanetService) {
   }
 
   ngOnInit(): void {
       if (this.id) {
-        this.planetService.getPlanetById(this.id).subscribe({
+        this.sub = this.planetService.getPlanetById(this.id).subscribe({
           next: (planet) => {
             this.planet = planet;
             this.error = '';
@@ -25,8 +27,17 @@ export class PlanetDetailComponent implements OnInit {
           error: (err) => {
             this.error = err.message;
             this.planet = undefined;
+          },
+          complete: () => {
+            this.log.log('Observable completo');
           }
         });
+      }
+  }
+  ngOnDestroy(): void {
+      if (this.sub) {
+        this.sub.unsubscribe();
+        this.sub = undefined;
       }
   }
 }
